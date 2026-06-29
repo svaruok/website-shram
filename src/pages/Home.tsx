cf9c1d962232c70ab5dbd73afd39e6df7f7325aa
svaruok <vincenzoop109@gmail.com>
2026-06-29 10:01:37 +0530

    Revert "Restore and animate 3D Colosseum on scroll"
    This reverts commit 01d836917d1adbc4633e038277145fff6633c242.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import TrustStats from '../components/TrustStats';
-import ColosseumPhilosophy from '../components/ColosseumPhilosophy';
 import Features from '../components/Features';
 import HowItWorks from '../components/HowItWorks';
 import Screenshots from '../components/Screenshots';
@@ -21,7 +20,6 @@ export default function Home() {
       <main className="flex-grow">
         <Hero />
         <TrustStats />
-        <ColosseumPhilosophy />
         <Features />
         <Screenshots />
         <HowItWorks />
